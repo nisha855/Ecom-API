0732d69023b72684a7762e50d5381fc5eb8a2db0
Nisha <nisha00396@gmail.com>
2021-09-06 21:38:04 +0530

    Created

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,12 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
- 
-const db=mongoose.connection;
-db.on("error",console.error.bind(console,"error connecting to db"));
-db.once("open",()=>console.log("connected to db"));
 
-module.exports=db;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Error connecting to DB"));
+db.once("open", () => console.log("Connected to DB"));
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-  const express=require("express"); 
-  const dotenv=require("dotenv").config();
-  const port=process.env.PORT;
-  const app=express();
- const db=require("./config/mongoose");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT;
+const app = express();
+const db = require("./config/mongoose");
 
 
 app.use(express.json());//package used in server(middleware function)
-  app.use("/",require('./routes'));  //it by default looks for index.js file only in routes folder
+app.use("/", require('./routes'));  //it by default looks for index.js file only in routes folder
 
 
-  //start server
-  app.listen(port,()=>console.log("server started",port));
+//start server
+app.listen(port, () => console.log("server started", port));
